200ae33d54dc523802b8b683282b82e0648e4c64
Ruslan Zharkov <zharkov.ruslan.e@gmail.com>
2021-02-20 23:20:15 +0300

    Added header

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { FaGamepad } from "react-icons/fa";
+
+import './Header.css';
 
 export const Header = () => {
-
+  return (
+    <div className="Header">
+      <div>
+        <FaGamepad size={35}/>
+        <div>
+          Games explorer
+        </div>
+      </div>
+    </div>
+  )
 }
